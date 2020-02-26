ed78866814a98aff882c5c83f5131bd90e7799bb
Kanuty <bartoszdudek2211@gmail.com>
2020-02-26 13:54:01 +0100

    'wip_stylization_of_components'

--- src/components/CheckboxPanel/CheckboxPanel.jsx
+++ src/components/CheckboxPanel/CheckboxPanel.jsx
@@ -1,27 +1,53 @@
 import React from 'react';
+import chaotic from '../../img/symbols/chaotic.png'
+import skavenish from '../../img/symbols/skavenish.png'
 import './style.css';
 
 const CheckboxPanel = (props) => {
   const {
-
+    logo
   } = props;
 
   return (
     <div className="panelBox">
       <div className="checkBox">
-        <div>
+        <div className="labelBox">
           <input
             type="checkbox"
-            id="party"
+            id="Chaos"
             name="Chaos"
-            />
-            <label htmlFor="party">Chaos</label>
+          />
+          <label htmlFor="Chaos">
+            <img src={chaotic}></img>
+          </label>
+            <div className="sliderBox">
+              <p>Chaos /place for slider value/</p>
+              <input
+                type="range"
+                className="slider"
+                min="1"
+                max="100"
+                id="ChaosSlider"
+                name="Chaos"
+              />
+            </div>
+        </div>
+      </div>
+      
+      <div className="checkBox">
+        <div className="labelBox">
+          <input
+            type="checkbox"
+            id="Skaven"
+            name="Skaven"
+          />
+          <label htmlFor="Skaven"><img src={skavenish}></img></label>
         </div>
         <div className="sliderBox">
           <input
             type="range"
             className="slider"
-            min="1" max="100" id="party" name="Chaos" />
+            min="1" max="100" id="party" name="Skaven" />
 
         </div>
       </div>
@@ -38,6 +64,11 @@ const CheckboxPanel = (props) => {
     </div>
   )
 };
+
+CheckboxPanel.defaultProps = {
+  logo: chaotic,
+};
+
 
 
 export default (CheckboxPanel);
