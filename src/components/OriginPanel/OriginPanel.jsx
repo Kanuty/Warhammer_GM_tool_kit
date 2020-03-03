976f503fce0a2cfe644879e9cc10b149628482e7
Kanuty <bartoszdudek2211@gmail.com>
2020-03-03 13:07:35 +0100

    'empire_and_bretonnia_has_been_add'

--- src/components/OriginPanel/OriginPanel.jsx
+++ src/components/OriginPanel/OriginPanel.jsx
@@ -31,15 +31,36 @@ const OriginPanel = (props) => {
   const {
 
   } = props;
-
+  const [bretonniaDisplay, setBretonniaDisplay] = useState(false);
   const [chaosDisplay, setChaosDisplay] = useState(true);
+  const [empireDisplay, setEmpireDisplay] = useState(false);
   const [humanKingdomsDisplay, setHumanKingdomsDisplay] = useState(false);
   const [othersDisplay, setOtherDisplay] = useState(false);
+  const toggleBretonniaDisplay = () => setBretonniaDisplay(!bretonniaDisplay);
   const toggleChaosDisplay = () => setChaosDisplay(!chaosDisplay);
+  const toggleEmpireDisplay = () => setEmpireDisplay(!empireDisplay);
   const toggleHumanKingdomsDisplay = () => setHumanKingdomsDisplay(!humanKingdomsDisplay);
   const toggleOthersDisplay = () => setOtherDisplay(!othersDisplay);
 
   // To do: When at least one OriginElement is active, than toggle button change collor.
+
+  const [valueOfBretonia , setValueOfBretonia] = useState(0);
+  const [valueOfAquitaine , setValueOfAquitaine] = useState(0);
+  const [valueOfArtois , setValueOfArtois] = useState(0);
+  const [valueOfBastonne , setValueOfBastonne] = useState(0);
+  const [valueOfBordeleaux , setValueOfBordeleaux] = useState(0);
+  const [valueOfBrionne , setValueOfBrionne] = useState(0);
+  const [valueOfCarcassonne , setValueOfCarcassonne] = useState(0);
+  const [valueOfCouronne , setValueOfCouronne] = useState(0);
+  const [valueOfCuileux , setValueOfCuileux] = useState(0);
+  const [valueOfGisoreux , setValueOfGisoreux] = useState(0);
+  const [valueOfGlanborielle , setValueOfGlanborielle] = useState(0);
+  const [valueOfLanguillie , setValueOfLanguillie] = useState(0);
+  const [valueOfLyonesse , setValueOfLyonesse] = useState(0);
+  const [valueOfMontfort , setValueOfMontfort] = useState(0);
+  const [valueOfMousillon , setValueOfMousillon] = useState(0);
+  const [valueOfParravon , setValueOfParravon] = useState(0);
+  const [valueOfQuenelles , setValueOfQuenelles] = useState(0);
 
   const [valueOfChaotic , setValueOfChaotic] = useState(0);
   const [valueOfNurlge , setValueOfNurgle] = useState(0);
@@ -52,7 +73,21 @@ const OriginPanel = (props) => {
   const [valueOfChaosdwarfs , setValueOfChaosdwarfs] = useState(0);
 
   const [valueOfImperium , setValueOfImperium] = useState(0);
-  const [valueOfBretonia , setValueOfBretonia] = useState(0);
+  const [valueOfAverland , setValueOfAverland] = useState(0);
+  const [valueOfHochland , setValueOfHochland] = useState(0);
+  const [valueOfMiddenland , setValueOfMiddenland] = useState(0);
+  const [valueOfNordland , setValueOfNordland] = useState(0);
+  const [valueOfOstermark , setValueOfOstermark] = useState(0);
+  const [valueOfOstland , setValueOfOstland] = useState(0);
+  const [valueOfReikland , setValueOfReikland] = useState(0);
+  const [valueOfSolland , setValueOfSolland] = useState(0);
+  const [valueOfStirland , setValueOfStirland] = useState(0);
+  const [valueOfSylvanic , setValueOfSylvanic] = useState(0);
+  const [valueOfTalabecland , setValueOfTalabecland] = useState(0);
+  const [valueOfWesterland , setValueOfWesterland] = useState(0);
+  const [valueOfWissenland , setValueOfWissenland] = useState(0);
+  const [valueOfThemoot , setValueOfThemoot] = useState(0);
+
   const [valueOfEstalia , setValueOfEstalia] = useState(0);
   const [valueOfKislev , setValueOfKislev] = useState(0);
   const [valueOfTilea , setValueOfTilea] = useState(0);
@@ -62,7 +97,6 @@ const OriginPanel = (props) => {
   const [valueOfNippon , setValueOfNippon] = useState(0);
   const [valueOfCathay , setValueOfCathay] = useState(0);
 
-  const [valueOfSylvanic , setValueOfSylvanic] = useState(0);
   const [valueOfKhemri , setValueOfKhemri] = useState(0);
   const [valueOfDwarfs , setValueOfDwarfs] = useState(0);
   const [valueOfGreenskins , setValueOfGreenskins] = useState(0);
@@ -76,7 +110,25 @@ const OriginPanel = (props) => {
 
   function handleChange () {
     props.onChange(
-    [ valueOfChaotic,
+    [ valueOfBretonia,
+      valueOfAquitaine,
+      valueOfArtois,
+      valueOfBastonne,
+      valueOfBordeleaux,
+      valueOfBrionne,
+      valueOfCarcassonne,
+      valueOfCouronne,
+      valueOfCuileux,
+      valueOfGisoreux,
+      valueOfGlanborielle,
+      valueOfLanguillie,
+      valueOfLyonesse,
+      valueOfMontfort,
+      valueOfMousillon,
+      valueOfParravon,
+      valueOfQuenelles,
+      
+      valueOfChaotic,
       valueOfNurlge,
       valueOfKhorne,
       valueOfTzeentch,
@@ -85,8 +137,23 @@ const OriginPanel = (props) => {
       valueOfNorsca,
       valueOfBeastmen,
       valueOfChaosdwarfs,
+
       valueOfImperium,
-      valueOfBretonia,
+      valueOfAverland,
+      valueOfHochland,
+      valueOfMiddenland,
+      valueOfNordland,
+      valueOfOstermark,
+      valueOfOstland,
+      valueOfReikland,
+      valueOfSolland,
+      valueOfStirland,
+      valueOfSylvanic,
+      valueOfTalabecland,
+      valueOfWesterland,
+      valueOfWissenland,
+      valueOfThemoot,
+
       valueOfEstalia,
       valueOfKislev,
       valueOfTilea,
@@ -95,7 +162,7 @@ const OriginPanel = (props) => {
       valueOfArraby,
       valueOfNippon,
       valueOfCathay,
-      valueOfSylvanic,
+
       valueOfKhemri,
       valueOfDwarfs,
       valueOfGreenskins,
@@ -110,42 +177,103 @@ const OriginPanel = (props) => {
   }
 
   useEffect( () => {handleChange()}, 
-    [ valueOfChaotic,
-      valueOfNurlge,
-      valueOfKhorne,
-      valueOfTzeentch,
-      valueOfSlaanesh,
-      valueOfSkaven,
-      valueOfNorsca,
-      valueOfBeastmen,
-      valueOfChaosdwarfs,
-      valueOfImperium,
-      valueOfBretonia,
-      valueOfEstalia,
-      valueOfKislev,
-      valueOfTilea,
-      valueOfBorderprinces,
-      valueOfInd,
-      valueOfArraby,
-      valueOfNippon,
-      valueOfCathay,
-      valueOfSylvanic,
-      valueOfKhemri,
-      valueOfDwarfs,
-      valueOfGreenskins,
-      valueOfLustria,
-      valueOfWoodelfs,
-      valueOfHighelfs,
-      valueOfDarkelfs,
-      valueOfOgres,
-      valueOfHobgoblins,
-      valueOfAlbion
-    ]
+  [ valueOfBretonia,
+    valueOfAquitaine,
+    valueOfArtois,
+    valueOfBastonne,
+    valueOfBordeleaux,
+    valueOfBrionne,
+    valueOfCarcassonne,
+    valueOfCouronne,
+    valueOfCuileux,
+    valueOfGisoreux,
+    valueOfGlanborielle,
+    valueOfLanguillie,
+    valueOfLyonesse,
+    valueOfMontfort,
+    valueOfMousillon,
+    valueOfParravon,
+    valueOfQuenelles,
+    
+    valueOfChaotic,
+    valueOfNurlge,
+    valueOfKhorne,
+    valueOfTzeentch,
+    valueOfSlaanesh,
+    valueOfSkaven,
+    valueOfNorsca,
+    valueOfBeastmen,
+    valueOfChaosdwarfs,
+
+    valueOfImperium,
+    valueOfAverland,
+    valueOfHochland,
+    valueOfMiddenland,
+    valueOfNordland,
+    valueOfOstermark,
+    valueOfOstland,
+    valueOfReikland,
+    valueOfSolland,
+    valueOfStirland,
+    valueOfSylvanic,
+    valueOfTalabecland,
+    valueOfWesterland,
+    valueOfWissenland,
+    valueOfThemoot,
+
+    valueOfEstalia,
+    valueOfKislev,
+    valueOfTilea,
+    valueOfBorderprinces,
+    valueOfInd,
+    valueOfArraby,
+    valueOfNippon,
+    valueOfCathay,
+
+    valueOfKhemri,
+    valueOfDwarfs,
+    valueOfGreenskins,
+    valueOfLustria,
+    valueOfWoodelfs,
+    valueOfHighelfs,
+    valueOfDarkelfs,
+    valueOfOgres,
+    valueOfHobgoblins,
+    valueOfAlbion
+  ]
   )
 
   return (
     <div className="panelBox">
       <h1>Origin</h1>
+        <button 
+          className="toggleButton" 
+          onClick={toggleBretonniaDisplay}>
+            { bretonniaDisplay? "Hide Bretonnia" : "Show Bretonnia" }
+        </button>
+        { bretonniaDisplay?
+          <div id="BretonniaContainer">
+            <OriginElement logo={bretonia} party="Bretonnia" onChange={setValueOfBretonia}/>
+            <OriginElement party="Aquitaine" onChange={setValueOfAquitaine}/>
+            <OriginElement party="Artois" onChange={setValueOfArtois}/>
+            <OriginElement party="Bastonne" onChange={setValueOfBastonne}/>
+            <OriginElement party="Bordeleaux" onChange={setValueOfBordeleaux}/>
+            <OriginElement party="Brionne" onChange={setValueOfBrionne}/>
+            <OriginElement party="Carcassonne" onChange={setValueOfCarcassonne}/>
+            <OriginElement party="Couronne" onChange={setValueOfCouronne}/>
+            <OriginElement party="Cuileux" onChange={setValueOfCuileux}/>
+            <OriginElement party="Gisoreux" onChange={setValueOfGisoreux}/>
+            <OriginElement party="Glanborielle" onChange={setValueOfGlanborielle}/>
+            <OriginElement party="L'anguillie" onChange={setValueOfLanguillie}/>
+            <OriginElement party="Lyonesse" onChange={setValueOfLyonesse}/>
+            <OriginElement party="Montfort" onChange={setValueOfMontfort}/>
+            <OriginElement party="Mousillon" onChange={setValueOfMousillon}/>
+            <OriginElement party="Parravon" onChange={setValueOfParravon}/>
+            <OriginElement party="Quenelles" onChange={setValueOfQuenelles}/>
+          </div>
+        :
+          <div/>}
+
         <button 
           className="toggleButton" 
           onClick={toggleChaosDisplay}>
@@ -165,6 +293,33 @@ const OriginPanel = (props) => {
           </div>
         : 
           <div/>}
+
+        <button 
+          className="toggleButton" 
+          onClick={toggleEmpireDisplay}>
+            { empireDisplay? "Hide Empire" : "Show Empire" }
+        </button>
+        { empireDisplay?
+          <div id="EmpireContainer">
+            <OriginElement logo={imperium} party="Empire" onChange={setValueOfImperium}/>
+            <OriginElement party="Averland" onChange={setValueOfAverland}/>
+            <OriginElement party="Hochland" onChange={setValueOfHochland}/>
+            <OriginElement party="Middenland" onChange={setValueOfMiddenland}/>
+            <OriginElement party="Nordland" onChange={setValueOfNordland}/>
+            <OriginElement party="Ostermark" onChange={setValueOfOstermark}/>
+            <OriginElement party="Ostland" onChange={setValueOfOstland}/>
+            <OriginElement party="Reikland" onChange={setValueOfReikland}/>
+            <OriginElement party="Solland" onChange={setValueOfSolland}/>
+            <OriginElement party="Stirland" onChange={setValueOfStirland}/>
+            <OriginElement logo={sylvanic} party="Sylvania" onChange={setValueOfSylvanic}/>
+            <OriginElement party="Talabecland" onChange={setValueOfTalabecland}/>
+            <OriginElement party="Westerland" onChange={setValueOfWesterland}/>
+            <OriginElement party="Wissenland" onChange={setValueOfWissenland}/>
+            <OriginElement party="The Moot" onChange={setValueOfThemoot}/>
+          </div>
+        :
+          <div/>}
+
         <button 
           className="toggleButton" 
           onClick={toggleHumanKingdomsDisplay}>
@@ -172,8 +327,6 @@ const OriginPanel = (props) => {
         </button>
         { humanKingdomsDisplay?
           <div id="humanKingdomsContainer">
-            <OriginElement logo={imperium} party="Empire" onChange={setValueOfImperium}/>
-            <OriginElement logo={bretonia} party="Bretonia" onChange={setValueOfBretonia}/>
             <OriginElement logo={estalia} party="Estalia" onChange={setValueOfEstalia}/>
             <OriginElement logo={kislev} party="Kislev" onChange={setValueOfKislev}/>
             <OriginElement logo={tilea} party="Tilea" onChange={setValueOfTilea}/>
@@ -185,6 +338,7 @@ const OriginPanel = (props) => {
          </div>
          :
           <div/> }
+
         <button 
           className="toggleButton" 
           onClick={toggleOthersDisplay}>
@@ -192,7 +346,6 @@ const OriginPanel = (props) => {
         </button>
         { othersDisplay?
           <div  id="othersContainer">
-            <OriginElement logo={sylvanic} party="Sylvania" onChange={setValueOfSylvanic}/>
             <OriginElement logo={khemri} party="Khemri" onChange={setValueOfKhemri}/>
             <OriginElement logo={dwarfs} party="Worlds Edge Mountains" onChange={setValueOfDwarfs}/>
             <OriginElement logo={greenskins} party="Badlands" onChange={setValueOfGreenskins}/>
@@ -206,6 +359,7 @@ const OriginPanel = (props) => {
           </div>
         :
           <div/> }
+
     </div>
   )
 };
